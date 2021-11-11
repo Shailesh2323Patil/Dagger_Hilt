0da415ab53235efb59108fbc69368527ee0bdf4f
Shailesh2323 <shailesh@abcdesigns.in>
2021-11-11 19:44:37 +0530

    Second commit

--- app/src/main/java/com/example/dagger_hilt/V9_Dagger_Hilt_Room/V9Application.kt
+++ app/src/main/java/com/example/dagger_hilt/V9_Dagger_Hilt_Room/V9Application.kt
@@ -3,6 +3,7 @@ package com.example.dagger_hilt.V9_Dagger_Hilt_Room
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+// Test Commit
 @HiltAndroidApp
 class V9Application : Application() {
 }
